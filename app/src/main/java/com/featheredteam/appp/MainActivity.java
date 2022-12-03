18205a7ed99d70c3ff174157055f77b5a8cd5393
MohamedSci <muhammedsaidsyed215@gmail.com>
2022-12-03 20:16:36 +0200

    33333

--- app/src/main/java/com/featheredteam/appp/MainActivity.java
+++ app/src/main/java/com/featheredteam/appp/MainActivity.java
@@ -1,28 +1,64 @@
 package com.featheredteam.appp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 import android.widget.TextView;
 
-import java.util.ArrayList;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity implements FragLst.ItemSelected {
 
     TextView tvDetail;
-     String [] arrLst;
+    String[] arrLst;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tvDetail = findViewById(R.id.tvDetail);
-        arrLst =getResources().getStringArray(R.array.descriptions);
+        arrLst = getResources().getStringArray(R.array.descriptions);
+
+        try{
+            if (findViewById(R.id.mainAc) != null) {
+                FragmentManager fm = this.getSupportFragmentManager();
+                fm.beginTransaction()
+                        .show(fm.findFragmentById(R.id.fragLst))
+                        .hide(fm.findFragmentById(R.id.fragDetail));
+            }
+            if (findViewById(R.id.mainAcLa) != null) {
+                FragmentManager fm2 = this.getSupportFragmentManager();
+                fm2.beginTransaction()
+                        .show(fm2.findFragmentById(R.id.fragDetail))
+                        .show(fm2.findFragmentById(R.id.fragLst));
+            }
+        } catch (Exception e){
+            System.out.println("eee 111111"+ e);
+        }
+
     }
 
 
     @Override
     public void onItemSelected(int i) {
+
         tvDetail.setText(arrLst[i]);
+        try{
+            if (findViewById(R.id.mainAc) != null) {
+                FragmentManager fm3 = this.getSupportFragmentManager();
+                fm3.beginTransaction()
+                        .hide(fm3.findFragmentById(R.id.fragLst))
+                        .show(fm3.findFragmentById(R.id.fragDetail));
+            } else {
+                FragmentManager fm4 = this.getSupportFragmentManager();
+                fm4.beginTransaction()
+                        .show(fm4.findFragmentById(R.id.fragLst))
+                        .show(fm4.findFragmentById(R.id.fragDetail));
+            }
+        } catch (Exception e){
+            System.out.println("eee 22222"+ e);
+        }
+
     }
 }
